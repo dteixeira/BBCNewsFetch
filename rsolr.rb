4bee11296bd013682ce4407616396f96dae5e3d1
Diogo Teixeira <diogo.andre.teixeira@gmail.com>
2013-11-06 10:16:10 +0000

    Solr server config added.

--- rsolr.rb
+++ rsolr.rb
@@ -4,5 +4,20 @@ require 'rsolr'
 solr = RSolr.connect :url => 'http://localhost:8080/solr'
 
 # send a request to /select
-response = solr.get 'select', :params => {:q => '*:*'}
-puts response
+# response = solr.get 'select', :params => {:q => 'text' }
+# puts response
+
+=begin
+File.open("./data.xml", "r") do |file|
+  solr.update :data => file.read
+end
+=end
+
+# response = solr.get 'select', :params => { :q => 'id:123456' }
+# if response['response']['numFound'] == 1
+#  solr.add :id => '123456', :title => 'This is my title, this is my text!', :topic => response['response']['docs'][0]['topic'] << 'world'
+# else
+#  solr.add :id => '123456', :title => 'This is my title, this is my text!', :topic => 'uk'
+# end
+solr.add :id => '123456', :title => 'I haz wifi.', :topic => 'cats'
+solr.commit
